f11a90d32a15fd294de0bcaddf887c8705f19671
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-03-28 13:59:50 -0600

    cambios 2

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
+import react,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-import {NavigationContainer, TabActions} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 
 import Ionicons from "react-native-vector-icons/Ionicons"
-function HomeScreen(){
+
+function LoginScreen({onPress}){
+  return(
+    <View style={styles.container}>
+      <StatusBar/>
+      <Text>Loguin Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function HomeScreen({onPress}){
 
   return(
     <View style={styles.container}>
-      <Text>Home Screem</Text>
+      <Text>Home Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>logOut</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -17,17 +35,30 @@ function HomeScreen(){
 function SettingsScreen(){
   return(
     <View style={styles.container}>
-      <Text>setting screen </Text>
+      <Text>Setting Screen </Text>
     </View>
   )
   }
 
   const tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator()
 
 export default function App() {
+  const [user,setUser]=useState(null);
   return (
     <NavigationContainer>
-      <tab.Navigator
+      {!user ? (
+        <Stack.Navigator>
+          <Stack.Screen
+          options={{headerShown:false}}
+          children={{props}=>(
+            <LoginScreen {...props} onPress={()=>setUser(true)} />
+          )}
+          name="Login"
+          />
+        </Stack.Navigator>
+      ):(
+        <tab.Navigator
       screenOptions={({route})=>({
         tabBarIcon:({focused, color, size})=>{
           let iconName;
@@ -44,9 +75,15 @@ export default function App() {
         tabBarInactiveTintColor:"gray",
       })}
       >
-        <tab.Screen name="Home" component={HomeScreen}/>
+        <tab.Screen
+         name="Home"
+         children={(props)=>(
+           <HomeScreen {...props} onPress={()=>setUser(null)}></HomeScreen>
+         )} 
+         />
         <tab.Screen name="Settings" component={SettingsScreen}/>
       </tab.Navigator>
+      )}
     </NavigationContainer>
   );
 }
