a7eb4e04ec7872aecfc981ebcdecf1f4d6eb447f
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-03-28 14:21:11 -0600

    estilos

--- App.js
+++ App.js
@@ -96,5 +96,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor:"#00a884",
+    width:150,
+    height:40,
+    borderRadius:10,
+    padding:5,
+    marginTop:10,
+  },
+  text:{
+    color:"#fff",
+  },
 });
 
