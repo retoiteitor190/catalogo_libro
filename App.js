e0352d964100bb8a8b9ffc07e406cc5a02e1b740
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-04-04 20:37:42 -0500

    creacion del navigator

--- App.js
+++ App.js
@@ -1,66 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-
-import {NavigationContainer} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
-import Ionicons from "react-native-vector-icons/Ionicons"
-
-import LoginScreen from "./src/pages/login"
-import SettingsScreen from './src/pages/Settings/Settings';
-import HomeScreen from './src/pages/Home/Home';
-
-
-
-
-
-  const tab = createBottomTabNavigator();
-  const Stack = createNativeStackNavigator()
+import MainNavigator from './src/Navigator/Navigator';
 
 export default function App() {
-  const [user,setUser]=useState(null);
-  return (
-    <NavigationContainer>
-      {!user ? (
-        <Stack.Navigator>
-          <Stack.Screen
-          options={{ headerShown: false }}
-          children={(props)=>(
-            <LoginScreen {...props} onPress={()=>setUser(true)} />
-          )}
-          name="Login"
-          />
-        </Stack.Navigator>
-      ):(
-        <tab.Navigator
-      screenOptions={({route})=>({
-        tabBarIcon:({focused, color, size})=>{
-          let iconName;
-          if(route.name == "Home"){
-            iconName = focused
-            ?"ios-information-circle"
-            :"ios-information-circle-outline";
-          }else if (route.name=="Settings"){
-            iconName = "ios-list";
-          } 
-          return <Ionicons name={iconName} size={size} color={color}/>
-        },
-        tabBarActiveTintColor:"red",
-        tabBarInactiveTintColor:"gray",
-      })}
-      >
-        <tab.Screen
-         name="Home"
-         children={(props)=>(
-           <HomeScreen {...props} onPress={()=>setUser(null)}></HomeScreen>
-         )} 
-         />
-        <tab.Screen name="Settings" component={SettingsScreen}/>
-      </tab.Navigator>
-      
-      )}
-    </NavigationContainer>
-  );
+
+  return <MainNavigator/>;
 }
 
 
